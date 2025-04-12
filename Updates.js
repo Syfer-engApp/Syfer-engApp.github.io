c6397c4a8fcb448a568c1458be0c11a4386481e6
Syfer-engApp <cheffpoopoop@gmail.com>
2025-04-11 21:12:49 -0500

    Update Updates.js

--- Updates.js
+++ Updates.js
@@ -1,1 +1,9 @@
-
+// Steam Clone Updates File
+const updateData = {
+  "latestVersion": "1.0.0",
+  "currentVersion": "1.0.0",
+  "updateAvailable": false,
+  "updateNotes": "This is the latest version of Steam Clone.",
+  "updateURL": "",
+  "critical": false
+};
