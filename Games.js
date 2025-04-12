fcb2abc5a9e60fe28eaf1fa953b551ef22e1c29e
Syfer-engApp <cheffpoopoop@gmail.com>
2025-04-11 21:11:59 -0500

    Update Games.js

--- Games.js
+++ Games.js
@@ -1,1 +1,27 @@
-
+// Steam Clone Online Game Data
+const onlineGameData = [
+  {
+    "name": "Red Dead Redemption 2",
+    "description": "An epic tale of life in America's unforgiving heartland. The game's vast and atmospheric world also provides the foundation for a brand new online multiplayer experience.",
+    "image_url": "",
+    "banner_url": "",
+    "download_url": "",
+    "size": "150 GB"
+  },
+  {
+    "name": "Grand Theft Auto V",
+    "description": "When a young street hustler, a retired bank robber, and a terrifying psychopath find themselves entangled with some of the most frightening and deranged elements of the criminal underworld.",
+    "image_url": "",
+    "banner_url": "",
+    "download_url": "",
+    "size": "105 GB"
+  },
+  {
+    "name": "Counter-Strike 2",
+    "description": "For over two decades, Counter-Strike has offered an elite competitive experience, one shaped by millions of players from across the globe.",
+    "image_url": "",
+    "banner_url": "",
+    "download_url": "",
+    "size": "25 GB"
+  }
+];
